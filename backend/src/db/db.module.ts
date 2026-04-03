b2a3aa456ef57cba699fcab932d0d9e48167dccc
Vanja <vanja_gigovic@hotmail.com>
2026-04-03 18:35:38 +0200

    add db module connection with db

---
+++ backend/src/db/db.module.ts
@@ -0,0 +1,21 @@
+import { Global, Module } from '@nestjs/common';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import { env } from '../config/env';
+
+@Global()
+@Module({
+  providers: [
+    {
+      provide: 'DATABASE_CONNECTION',
+      useFactory: () => {
+        const pool = new Pool({
+          connectionString: env.DATABASE_URL,
+        });
+        return drizzle(pool);
+      },
+    },
+  ],
+  exports: ['DATABASE_CONNECTION'],
+})
+export class DbModule {}
