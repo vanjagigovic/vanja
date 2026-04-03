10c527a5dac0bc7857d62ffdbb241720e50e7f2d
Vanja <vanja_gigovic@hotmail.com>
2026-04-03 18:53:33 +0200

    db module connection

--- backend/src/db/db.module.ts
+++ backend/src/db/db.module.ts
@@ -3,6 +3,11 @@ import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
 import { env } from '../config/env';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+if (!env.DATABASE_URL) {
+  throw new Error('DATABASE_URL environment variable is not defined');
+}
+
 @Global()
 @Module({
   providers: [
@@ -10,6 +15,7 @@ import { env } from '../config/env';
       provide: 'DATABASE_CONNECTION',
       useFactory: () => {
         const pool = new Pool({
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
           connectionString: env.DATABASE_URL,
         });
         return drizzle(pool);
