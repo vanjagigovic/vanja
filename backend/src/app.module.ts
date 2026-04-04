6917271e6715e27f6fd2b0da595028ed1c486f62
Vanja <vanja_gigovic@hotmail.com>
2026-04-04 18:54:15 +0200

    update app.module - event module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { EventsModule } from './events/event.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [EventsModule],
 })
 export class AppModule {}
