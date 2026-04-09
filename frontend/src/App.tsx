15af9931cfefedb90d0703695a08fca8be6ff41e
Vanja <vanja_gigovic@hotmail.com>
2026-04-09 20:08:52 +0200

    added routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,18 @@
 import { Calendar } from "./components/Calendar";
+import { formatDateKey } from "./utils/calendar-utils";
+
+function CalendarRedirect() {
+  return (
+    <Navigate to={`/calendar/month/${formatDateKey(new Date())}`} replace />
+  );
+}
 
 export function App() {
-  return <Calendar />;
-};
+  return (
+    <Routes>
+      <Route path="/" element={<CalendarRedirect />} />
+      <Route path="/calendar/:view/:date" element={<Calendar />} />
+      <Route path="*" element={<CalendarRedirect />} />
+    </Routes>
+  );
+}
