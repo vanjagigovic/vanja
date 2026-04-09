0eb83a7459ac8432e50555bb8c76b0fff782ef00
Vanja <vanja_gigovic@hotmail.com>
2026-04-10 01:11:41 +0200

    added reminder icon

--- frontend/src/components/CalendarEventChip.tsx
+++ frontend/src/components/CalendarEventChip.tsx
@@ -4,6 +4,7 @@ import type { CalendarEvent } from "../types/types";
 import { Chip, useTheme} from "@mui/material";
 import { eventMotionProps } from "../helpers/motion-presets";
 import { monthEventChipSx } from "../styles/eventStyles";
+import { NotificationsActiveOutlined } from "@mui/icons-material";
 
 interface CalendarEventChipProps {
     event: CalendarEvent;
@@ -11,15 +12,16 @@ interface CalendarEventChipProps {
     onClick: () => void;
 }
 
+
 export function CalendarEventChip({ event, isMobile, onClick }: CalendarEventChipProps) {
     const theme = useTheme();
     const colors = getEventTypeColors(theme, event.eventType);
-
     return (
         <motion.div {...eventMotionProps}>
             <Chip
                 label={event.title}
                 size='small'
+                icon={event.reminderEnabled ? <NotificationsActiveOutlined /> : undefined }
                 onClick={(e) => {e.stopPropagation()
                 onClick();
             }}
