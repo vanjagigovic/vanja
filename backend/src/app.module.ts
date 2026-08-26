5452e6af127833ff3d7e30249be0813ca6a1e57e
vanjagigovic <65625302+vanjagigovic@users.noreply.github.com>
2026-08-26 15:40:41 +0200

    added auth model

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { EventsModule } from './events/event.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [EventsModule],
+  imports: [AuthModule, EventsModule],
 })
 export class AppModule {}
